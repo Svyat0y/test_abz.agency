6182302dff47dd38d9572b6029db1fe1f1b725ef
shurik <86realmadrid86@gmail.com>
2022-08-13 17:18:42 +0300

    added layout for the upload file block and started using with formik

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,58 +1,81 @@
-import styles from './Form.module.scss'
+import styles     from './Form.module.scss'
+import { Formik } from 'formik'
 
 
 const Form = () => {
+
+	const initialValues = {
+		name: '',
+		email: '',
+		radio: '',
+	}
+	const onSubmit = (values, onSubmitProps) => {
+		console.log(onSubmitProps)
+		onSubmitProps.resetForm()
+	}
+
+
 	return (
 		<div className={ styles.form }>
 			<h2 className='title'>Working with POST request</h2>
-			<form>
-				<div className={ styles.form__input }>
-					<label htmlFor='name'>
-						<input type='text' id='name' name='name' placeholder='Your name'/>
-					</label>
-				</div>
+			<Formik
+				initialValues={ initialValues }
+				onSubmit={ onSubmit }>
 
-				<div className={ styles.form__input }>
-					<label htmlFor='email'>
-						<input type='email' name='email' id='email' placeholder='Email'/>
-					</label>
-				</div>
-
-				<div className={ styles.form__input }>
-					<label htmlFor='phone'>
-						<input type='tel' name='phone' id='phone' placeholder='Phone'/>
-					</label>
-					<span>+38(XXX) XXX - XXX - XX</span>
-				</div>
-
-				<div className={ styles.form__radios }>
-					<span>Select Your position</span>
-
-					<div className={ styles.form__radio }>
-						<label>
-							<input type='radio' name='option1' id='1'/>
-							<span className={ styles.form__radioIndicator }></span>
-							<span>Frontend developer</span>
-						</label>
-						<label>
-							<input type='radio' name='option1' id='2'/>
-							<span className={ styles.form__radioIndicator }></span>
-							<span>Backend developer</span>
-						</label>
-						<label>
-							<input type='radio' name='option1' id='3'/>
-							<span className={ styles.form__radioIndicator }></span>
-							<span>Designer</span>
-						</label>
-						<label>
-							<input type='radio' name='option1' id='4'/>
-							<span className={ styles.form__radioIndicator }></span>
-							<span>QA</span>
+				<form>
+					<div className={ styles.form__input }>
+						<label htmlFor='name'>
+							<input type='text' id='name' name='name' placeholder='Your name'/>
 						</label>
 					</div>
-				</div>
-				<button disabled={ true } className='btn' type='submit'>Sign up</button>
-			</form>
+
+					<div className={ styles.form__input }>
+						<label htmlFor='email'>
+							<input type='email' name='email' id='email' placeholder='Email'/>
+						</label>
+					</div>
+
+					<div className={ styles.form__input }>
+						<label htmlFor='phone'>
+							<input type='tel' name='phone' id='phone' placeholder='Phone'/>
+						</label>
+						<span>+38(XXX) XXX - XXX - XX</span>
+					</div>
+
+					<div className={ styles.form__radios }>
+						<span>Select Your position</span>
+
+						<div className={ styles.form__radio }>
+							<label>
+								<input defaultChecked type='radio' name='option1' id='1'/>
+								<span className={ styles.form__radioIndicator }></span>
+								<span>Frontend developer</span>
+							</label>
+							<label>
+								<input type='radio' name='option1' id='2'/>
+								<span className={ styles.form__radioIndicator }></span>
+								<span>Backend developer</span>
+							</label>
+							<label>
+								<input type='radio' name='option1' id='3'/>
+								<span className={ styles.form__radioIndicator }></span>
+								<span>Designer</span>
+							</label>
+							<label>
+								<input type='radio' name='option1' id='4'/>
+								<span className={ styles.form__radioIndicator }></span>
+								<span>QA</span>
+							</label>
+						</div>
+					</div>
+					<div className={ styles.form__uploadFile }>
+						<label className={ styles.form__customBtn } htmlFor='file'>Upload</label>
+						<input className={ styles.form__inputBtn } type='file' id='file'/>
+						<span className={ styles.form__uploadText }>Upload your photo</span>
+					</div>
+					<button className={ `btn ${ styles.formBtn }` } type='submit'>Sign up</button>
+				</form>
+			</Formik>
 		</div>
 	)
 }
